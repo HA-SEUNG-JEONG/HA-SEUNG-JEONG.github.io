703755d09a82db4806b97f7ef5f60b128f8253ec
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-31 16:01:17 +0900

    chore : about 페이지 빈 페이지로 만들기

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../layout';
-import Seo from '../components/seo';
-import Bio from '../components/bio';
-import TimeStampSection from '../components/timestamp-section';
-import ProjectSection from '../components/project-section';
+// import Seo from '../components/seo';
+// import Bio from '../components/bio';
+// import TimeStampSection from '../components/timestamp-section';
+// import ProjectSection from '../components/project-section';
 
 function AboutPage({ data }) {
-  const metaData = data.site.siteMetadata;
-  const { author, about, language } = metaData;
-  const { timestamps, projects } = about;
+  // const metaData = data.site.siteMetadata;
+  // const { author, about, language } = metaData;
+  // const { timestamps, projects } = about;
   return (
     <Layout>
-      <Seo title="About" />
+      {/* <Seo title="About" />
       <Bio author={author} language={language} />
       <TimeStampSection timestamps={timestamps} />
-      <ProjectSection projects={projects} />
+      <ProjectSection projects={projects} /> */}
     </Layout>
   );
 }
