6781321b7933e3f8eea63e651d102c6e66fd9a00
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-04-13 22:09:17 +0900

    Chore : 사이트 타이틀 변경

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomePage({ data }) {
 
   return (
     <Layout>
-      <Seo title="Home" />
+      <Seo title="정하승의 블로그" />
       <Bio author={author} language={language} />
       <PostTabs
         posts={posts}
